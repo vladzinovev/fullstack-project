4117d9c3aa1261f1e77906008803b5710bd7c54a
Vladislav <vladozdeveloper@gmail.com>
2022-10-06 21:29:22 +0300

    receive and read incoming data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+//научили читать json запросы
+app.use(express.json())
+
 //если приходит get запрос отправляем привет мир (что передаем клиенту)
 app.get('/',(req,res)=>{
     res.send('Hello world!');
@@ -9,6 +12,8 @@ app.get('/',(req,res)=>{
 
 //если придет post запрос
 app.post('/auth/login',(req,res)=>{
+    //считываем полученные данные
+    console.log(req.body);
     res.json({
         succes:true,
     })
