4da1d908eff255b459569d20f88130d062a04be5
Vladislav <vladozdeveloper@gmail.com>
2022-10-11 12:42:59 +0300

    created an authorization window

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -3,26 +3,70 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import {useForm} from 'react-hook-form';
+import { useDispatch , useSelector} from "react-redux";
+import {Navigate} from "react-router-dom";
+import {fetchAuth, selectIsAuth} from '../../redux/slices/auth';
 
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register, 
+    handleSubmit, 
+    setError, 
+    fromState:{errors,isValid},
+  }=useForm({
+    defaultValues:{
+      email:'',
+      password:'',
+    },
+    //валидация будет происходить если эти два поля поменялись
+    mode:'onChange'
+  });
+
+
+  const onSubmit=(values)=>{
+    dispatch(fetchAuth(values))
+  };
+
+  //если мы авторизовнаы, то переходим на главную
+  if (isAuth){
+    return <Navigate to='/'/>
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          //будет подсвечиваться красным
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          //чтобы браузер подсказывал что надо email@.ru ввести
+          type="email"
+          {...register('email',{required:'Укажите почту'})}
+          fullWidth
+        />
+        <TextField 
+          className={styles.field} 
+          label="Пароль" 
+          //будет подсвечиваться красным
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password',{required:'ВВедите пароль'})}
+          fullWidth 
+        />
+        <Button type='submit' size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
---
+++ frontend/src/redux/slices/auth.js
@@ -0,0 +1,37 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchAuth=createAsyncThunk('auth/fetchUserData',async (params) =>{
+    const {data} = await axios.post('/auth/login', params);
+    return data;
+})
+
+const initialState={
+    items:null,
+    status:'loading'
+}
+
+const authSlice=createSlice({
+    name:'auth',
+    initialState,
+    extraReducers:{
+        [fetchAuth.pending]:(state)=>{
+            state.status='loading';
+            state.data=null;
+        },
+        [fetchAuth.fulfilled]:(state,action)=>{
+            state.status='loaded';
+            state.data=action.payload;
+        },
+        [fetchAuth.rejected]:(state)=>{
+            state.status='error';
+            state.data=null;
+        },
+    }
+})
+
+// булевое значение об авторизации
+export const selectIsAuth=(state)=>Boolean(state.auth.data);
+
+
+export const authReducer=authSlice.reducer;
